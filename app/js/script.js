0ca9d8330685e54cec64e7ef8f65fd6fb694dc75
michael-goncharov <mic.goncharov@gmail.com>
2018-08-24 16:45:45 +0300

    Why you choose us section

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,19 @@
 $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
-        items: 4,
+        responsive:{
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            },
+            1180:{
+                items:4
+            }
+        },
         autoplay: true,
         autoplayTimeout: 2000,
         autoplaySpeed: 1000,
